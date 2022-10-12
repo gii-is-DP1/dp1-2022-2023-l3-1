e932c0b5566b9854c8d7d320b6e862d77853ff0c
Juan <juanmc015@gmail.com>
2022-10-12 18:19:55 +0200

    Achievements y Game

---
+++ src/main/java/org/springframework/samples/petclinic/statistics/AchievementService.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.statistics;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AchievementService  {
+    
+    AchievementRepository repo;
+
+    @Autowired
+    public AchievementService(AchievementRepository repo){
+        this.repo=repo;
+    }
+
+    List<Achievement> getAchievements(){
+        return repo.findAll();
+    }
+
+    public void deleteAchievementByid(int id){
+        repo.deleteById(id);
+    }
+
+    public Achievement getById(int id){
+        return repo.findById(id).get();
+    }
+
+    public void save(Achievement achievement){
+        repo.save(achievement);
+    }
+}
