5265536ce26f418f6f93c31b3f199a4465d0ea8e
Juan <juanmc015@gmail.com>
2022-10-30 15:54:44 +0100

    Añadido clase Player

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -39,6 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/session/**").permitAll()
 				.antMatchers("/admin/**").hasAnyAuthority("admin")
 				.antMatchers("/statistics/**").hasAnyAuthority("admin")
+				.antMatchers("/players/**").hasAnyAuthority("admin")
 				.antMatchers("/games/**").hasAnyAuthority("admin")
 				.antMatchers("/owners/**").hasAnyAuthority("owner","admin")				
 				.antMatchers("/vets/**").authenticated()
