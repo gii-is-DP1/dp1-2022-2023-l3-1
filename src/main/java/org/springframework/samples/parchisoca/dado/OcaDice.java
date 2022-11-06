9ffc6f540dc101417ddf887502b23ee720b3e300
Juan <juanmc015@gmail.com>
2022-11-06 19:55:31 +0100

    fixes

---
+++ src/main/java/org/springframework/samples/parchisoca/dado/OcaDice.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.parchisoca.dado;
+
+import javax.persistence.Entity;
+
+import org.springframework.samples.parchisoca.model.BaseEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class OcaDice extends BaseEntity{
+    private Integer dice;
+    private final Integer MAX = 6;
+
+    public void rollDice() {
+
+        this.dice = (int) (Math.random() * MAX ) + 1;
+    }
+
+}
