e8310241389e09547b04f054a675cf38888d4a5b
alvcarber1 <acarrerabernal@gmail.com>
2022-11-08 19:16:01 +0100

    boxesOca

--- src/main/java/org/springframework/samples/parchisoca/board/BoxesOca.java
+++ src/main/java/org/springframework/samples/parchisoca/board/BoxesOca.java
@@ -1,42 +1,27 @@
 package org.springframework.samples.parchisoca.board;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
+import javax.persistence.Entity;
+
+import org.springframework.samples.parchisoca.model.BaseEntity;
 import org.springframework.samples.parchisoca.piece.OcaPiece;
 
-public class BoxesOca {
+import lombok.Getter;
+import lombok.Setter;
 
-    List<OcaPiece> ocaPieces;
+
+@Entity
+@Getter
+@Setter
+public class BoxesOca extends BaseEntity {
+    int numberBoxes;
+    private SpecialBoxesOca specialBoxOca;
+
+    public BoxesOca(SpecialBoxesOca oca) {
+        this.specialBoxOca = oca;
+    }
+
     
-    // public BoxesOca(SpecialBoxesOca box) {
-    //     return this.box = box;
-    // }
-
-    // public void posicionActual() {
-    //     List<OcaPiece> boxesWhites = new ArrayList<>(63);
-    //     for (int i=0; i<68; i++){
-    //         BoxesOca res;
-    //         if(i==5 || i==9 || i==14 || i==18 || i== 23 || i==27 
-    //         || i== 32 || i==36 || i==41 || i==45 || i==50 || i==54 || i== 59){
-    //             res = new BoxesOca(SpecialBoxesOca.OCA);
-    //         } else if (i==6 || i==12) {
-    //             res = new BoxesOca(SpecialBoxesOca.BRIDGE);
-    //         } else if (i==26 || i==53) {
-    //             res = new BoxesOca(SpecialBoxesOca.DICES);
-    //         } else if (i==19) {
-    //             res = new BoxesOca(SpecialBoxesOca.HOSTAL);
-    //         } else if (i==31) {
-    //             res = new BoxesOca(SpecialBoxesOca.WELL);
-    //         } else if (i==42) {
-    //             res = new BoxesOca(SpecialBoxesOca.LABYRINTH);
-    //         } else if (i==58) {
-    //             res = new BoxesOca(SpecialBoxesOca.DEATH);
-    //         } else if (i==63) {
-    //             res = new BoxesOca(SpecialBoxesOca.GOAL);
-    //         } else {
-    //             res = new BoxesOca(SpecialBoxesOca.NORMAL);
-    //         }
-    //     }
-    // };
 }
--- src/main/java/org/springframework/samples/parchisoca/board/OcaBoard.java
+++ src/main/java/org/springframework/samples/parchisoca/board/OcaBoard.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.parchisoca.board;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -26,6 +27,8 @@ import lombok.Setter;
 @Table(name = "oca_boards")
 public class OcaBoard extends BaseEntity {
 
+    @OneToMany
+    private List<BoxesOca> boxesOcaNormal;
 
     String background;
 
@@ -45,8 +48,40 @@ public class OcaBoard extends BaseEntity {
         this.height = 800;
     
     }  
+
+    public void  posicionActual() {
+        boxesOcaNormal = new ArrayList<BoxesOca>(63);
+        for (int i=0; i<63; i++){
+            BoxesOca res;
+            if(i==5 || i==9 || i==14 || i==18 || i== 23 || i==27 
+            || i== 32 || i==36 || i==41 || i==45 || i==50 || i==54 || i== 59){
+                res = new BoxesOca(SpecialBoxesOca.OCA);
+            } else if (i==6 || i==12) {
+                res = new BoxesOca(SpecialBoxesOca.BRIDGE);
+            } else if (i==26 || i==53) {
+                res = new BoxesOca(SpecialBoxesOca.DICES);
+            } else if (i==19) {
+                res = new BoxesOca(SpecialBoxesOca.HOSTAL);
+            } else if (i==31) {
+                res = new BoxesOca(SpecialBoxesOca.WELL);
+            } else if (i==42) {
+                res = new BoxesOca(SpecialBoxesOca.LABYRINTH);
+            } else if (i==58) {
+                res = new BoxesOca(SpecialBoxesOca.DEATH);
+            } else if (i==63) {
+                res = new BoxesOca(SpecialBoxesOca.GOAL);
+            } else {
+                res = new BoxesOca(SpecialBoxesOca.NORMAL);
+            }
+            boxesOcaNormal.add(res);
+        }
+    }
+
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "ocaBoard",fetch = FetchType.EAGER)
     List<OcaPiece> pieces; 
+
+    // @OneToMany
+    // List<BoxesOca> boxesOcaNormal; 
 }
 
 
