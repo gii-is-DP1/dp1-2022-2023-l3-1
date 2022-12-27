8c2e902e572b38a19cd6f555361df53c1bf933e8
edubusluc <edubusluc@alum.us.es>
2022-12-27 21:27:46 +0100

    Fix minor errors

--- src/test/java/org/springframework/samples/parchisoca/oca/BoxesOcaServiceTest.java
+++ src/test/java/org/springframework/samples/parchisoca/oca/BoxesOcaServiceTest.java
@@ -1,25 +1,22 @@
 package org.springframework.samples.parchisoca.oca;
 
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.parchisoca.Oca.BoxesOca;
+import org.springframework.samples.parchisoca.Oca.BoxesOcaService;
+import org.springframework.samples.parchisoca.Oca.SpecialBoxesOca;
 import org.springframework.samples.parchisoca.board.OcaBoard;
 import org.springframework.samples.parchisoca.board.OcaBoardService;
-import org.springframework.samples.parchisoca.board.ParchisBoard;
-import org.springframework.samples.parchisoca.board.ParchisBoardService;
-import org.springframework.samples.parchisoca.player.Player;
-import org.springframework.samples.parchisoca.player.PlayerService;
+
 
 import org.springframework.stereotype.Service;
-
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
-import javax.validation.ConstraintViolationException;
+
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class BoxesOcaServiceTest {
