6464347e41331e66727ff77ff90df3f915dcd4c6
edubusluc <edubusluc@alum.us.es>
2023-01-04 12:29:59 +0100

    First Step in ParchisBoardService Test

--- src/test/java/org/springframework/samples/parchisoca/board/OcaBoardServiceTest.java
+++ src/test/java/org/springframework/samples/parchisoca/board/OcaBoardServiceTest.java
@@ -1,8 +1,11 @@
 package org.springframework.samples.parchisoca.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.parchisoca.dice.OcaDice;
+import org.springframework.samples.parchisoca.dice.OcaDiceService;
 import org.springframework.samples.parchisoca.game.Game;
 import org.springframework.samples.parchisoca.piece.OcaPiece;
 import org.springframework.samples.parchisoca.piece.OcaPieceService;
 import org.springframework.samples.parchisoca.player.Player;
+import org.springframework.samples.parchisoca.player.PlayerService;
 import org.springframework.stereotype.Service;
 
 
@@ -26,17 +31,35 @@ public class OcaBoardServiceTest {
     @Autowired
     OcaPieceService ocaPieceService;
 
+    @Autowired
+    OcaDiceService ocaDiceService;
+
+    @Autowired
+    PlayerService playerService;
+
     OcaBoard ocaBoard = new OcaBoard();
 
     Player player = null;
 
-    OcaDice ocaDice = null;
+    OcaDice ocaDice = new OcaDice();
 
     OcaPiece ocaPiece = null;
 
 
     @BeforeEach
     void setUp(){
+
+        player = playerService.findById(1);
+        ocaDice.setPlayer(player);
+        ocaDice.setOcaBoard(ocaBoard);
+        ocaDiceService.save(ocaDice);
+        List<OcaDice> ls = new ArrayList<>();
+        ls.add(ocaDice);
+
+       
+        player.setOcaDice(ls);
+        playerService.save(player);
+
         ocaBoard.setBoxes(new ArrayList<>());
         ocaBoard.setGame(new Game());
         ocaBoard.setPieces(new ArrayList<OcaPiece>());
@@ -53,6 +76,14 @@ public class OcaBoardServiceTest {
       OcaBoard ocaBoardTest = ocaBoardService.findById(ocaBoard.getId());
       assertThat(ocaBoardTest.getBackground().equals(ocaBoard.getBackground()));
     }
+
+    // @Test
+    // void shouldFindOcaDiceByPlayerAndBoard(){
+    //   ocaBoardService.save(ocaBoard);
+    //   OcaDice d = ocaBoardService.findOcaDiceByPlayerAndBoard(player, ocaBoard);
+    //   assertTrue(d != null);
+      
+    // }
 
 
 
